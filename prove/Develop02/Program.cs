4032bf056ab9cf4590870959e0b56f7b792fcb7f
Arturosilfa <arturosilfa06@gmail.com>
2024-09-29 00:14:53 -0400

    class done

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace JournalProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+            bool continueRunning = true;
+
+            while (continueRunning)
+            {
+                Console.WriteLine("Please select one of the following options: ");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display");
+                Console.WriteLine("3. Load");
+                Console.WriteLine("4. Save");
+                Console.WriteLine("5. Quit");
+
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        journal.AddEntry();
+                        break;
+
+                    case "2":
+                        journal.DisplayEntries();
+                        break;
+
+                    case "3":
+                        journal.LoadFromFile();
+                        break;
+
+                    case "4":
+                        journal.SaveToFile();
+                        break; 
+
+                    case "5":
+                        continueRunning = false;
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Invalid option. Choose again.");
+                        break;
+                }
+            }
+        }
     }
 }
